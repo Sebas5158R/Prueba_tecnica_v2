001644b2958a56ac63ec3ec16fe569abdfe9b2f1
Julian <sihjulian@hotmail.com>
2024-02-28 18:00:42 -0500

    el jugador

--- src/components/UsingFA.jsx
+++ src/components/UsingFA.jsx
@@ -7,7 +7,7 @@ const UsingFA= ()=>{
 
     const dispatch = useDispatch();
     const  user =  useSelector((state) => state.users_from_db.userLog);
-    console.log(user+"este es el usuario en using")
+   console.log(user.using2FA)
     const [formData, setFormData] = useState({
         names:"",
         lastNames: "",
@@ -24,7 +24,25 @@ const UsingFA= ()=>{
                 roleType: ""
             }
         ]
-    });
+    });useEffect(() => {
+        if (user) {
+            setFormData({
+                names: user.names,
+                lastNames: user.lastNames,
+                email: user.email,
+                documentType: user.documentType,
+                documentNumber: user.documentNumber,
+                phoneNumber: user.phoneNumber,
+                legal_person: user.legal_person,
+                isUsing2FA:isFA,
+                password: user.password,
+                roles: user.roles.map(role => ({
+                    idRole: role.idRole,
+                    roleType: role.roleType
+                }))
+            });
+        }
+    }, [user]);
 
     // Verificar si el usuario está disponible antes de renderizar
     useEffect(() => {
@@ -36,87 +54,75 @@ const UsingFA= ()=>{
         dispatch(userByEmail({email:emailS}))
     }, [dispatch]);
 
-    useEffect(() => {
-        if (user) {
-            setFormData({
-                names: user.names,
-                lastNames: user.lastNames,
-                email: user.email,
-                documentType: user.documentType,
-                documentNumber: user.documentNumber,
-                phoneNumber: user.phoneNumber,
-                legal_person: user.legal_person,
-                isUsing2FA: user.isUsing2FA,
-                password: user.password,
-                roles: user.roles.map(role => ({
-                    idRole: role.idRole,
-                    roleType: role.roleType
-                }))
-            });
-        }
-    }, [user]);
+    const   [isFA,setisFA] = useState(user.using2FA)
+
 
     const handleChange = (event) => {
-        if (event.target.name === "isUsingFA") {
-            formData.isUsing2FA(event.target.checked);
-        }
-        else {
             const {name, value} = event.target;
             setFormData({
                     ...formData,
                     [name]: value
                 }
             );
-        }
-        console.log(formData);
+    }
+
+    const  handleFA=()=>{
+        console.log(isFA)
+        setisFA(!isFA)
 
     }
+
+
+   const  handleFA1 =() =>{
+        handleFA()
+        console.log("++++++++++++++")
+    console.log(isFA)
+        console.log("++++++++++++++")
+
+       console.log("000000000000")
+       console.log(isFA)
+       console.log("000000000000")
+
+        console.log(isFA+ "esto es el using")
+        setFormData({names: user.names,
+            lastNames: user.lastNames,
+            email: user.email,
+            documentType: user.documentType,
+            documentNumber: user.documentNumber,
+            phoneNumber: user.phoneNumber,
+            legal_person: user.legal_person,
+            isUsing2FA:isFA,
+            password: user.password,
+            roles: user.roles.map(role => ({
+                idRole: role.idRole,
+                roleType: role.roleType
+            }))})
+        console.log("2222222")
+     console.log(formData)
+       console.log(formData.isUsing2FA)
+        console.log("2222222")
+
+    };
+
+const envio = ()=>{
+    dispatch(updateUser({id:user.idUser,userData:formData}));
+}
+
     const handleSubmit = (event) => {
         event.preventDefault();
         dispatch(updateUser({id:user.idUser,userData:formData}));
     }
-
     console.log(user)
 
-    const [enabled, setEnabled] = useState(user.isUsing2FA);
-    console.log(enabled +"este es el estado")
 
       return (
-      <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-          <div className="flex">
-              <label className="inline-flex relative items-center mr-5 cursor-pointer">
-                  <input
-                      type="checkbox"
-                      className="sr-only peer"
-                      name="isUsingFA"
-                      value={enabled}
-                      checked={enabled}
-
-
-
-
-
-
-
-
-
-
-
-
-                                           onChange={() => setEnabled(!enabled)}
-                  />
-                  <div
-                      onClick={() => {
-                          setEnabled(!enabled);
-                      }}
-                      className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-green-300 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
-                  ></div>
-                  <span className="ml-2 text-sm font-medium text-gray-900">
-                        ON
-                    </span>
-              </label>
-          </div>
-      </div>
+        <div>
+            <form>
+                <button type="button"    onClick={handleFA1}    name={"isUsingFA"} onChange={handleChange}
+                        className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "> $
+                </button>
+            </form>
+        </div>
       );
 
 }
